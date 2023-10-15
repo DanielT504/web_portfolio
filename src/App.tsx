f6ed4d64ff1c18fe4a09816ab929ada6486f3689
DanielT504 <62156098+DanielT504@users.noreply.github.com>
2023-10-14 22:15:31 -0400

    blurb update

--- src/App.tsx
+++ src/App.tsx
@@ -119,9 +119,9 @@ function App({mode}: AppProps) {
 
         {isVisible &&
           <div className="blurb">
-              I'm a 4th year undergrad at uWaterloo,
-              experienced with embedded systems and object oriented code,
-              with an interest in full stack and back end dev. <br/><br/>My contacts and resume are linked
+              I'm a 4th year undergrad at uWaterloo, 
+              experienced with OOP, backend/full-stack, 
+              and embedded systems, with a passion for AI/ML. <br/><br/>My contacts and resume are linked
               below if you'd like to reach out!
           </div>
         }
